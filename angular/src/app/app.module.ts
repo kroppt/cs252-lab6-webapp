89f501294d230ed5194621c0344af17f66dd3be4
Tyler Kropp <kroppt@purdue.edu>
2017-12-06 01:27:05 -0500

    Add toolbar and Angular Material style

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,18 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import 'hammerjs';
 
+import {
+  MatButtonModule,
+  MatIconModule,
+  MatToolbarModule
+} from '@angular/material';
+
+@NgModule({
+  exports: [
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule
+  ]
+})
+export class MaterialModule {}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
