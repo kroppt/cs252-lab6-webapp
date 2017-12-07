d96acee1c08d11ae8d98fabfdfe57a08087f6ec0
Tyler Kropp <kroppt@purdue.edu>
2017-12-07 02:44:02 -0500

    Add new pages to app.module

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -2,9 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { AccountComponent } from './account/account.component';
+import { NewEntryComponent } from './new-entry/new-entry.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 import { HomeComponent } from './home/home.component';
 import { SignupComponent } from './signup/signup.component';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 import 'hammerjs';
 
 import {
@@ -22,6 +27,34 @@ import {
 })
 export class MaterialModule {}
 
+const appRoutes: Routes = [
+  {
+    path: 'account/:id',
+    component: AccountComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'newentry',
+    component: NewEntryComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  {
+    path: '',
+    redirectTo: '/',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+  }
+];
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -32,7 +65,11 @@ export class MaterialModule {}
   imports: [
     BrowserModule,
     HttpModule,
-    MaterialModule
+    MaterialModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
