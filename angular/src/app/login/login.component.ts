73a9caacb71d6fefc49995e5d428fcb601bc2fa4
Tyler Kropp <kroppt@purdue.edu>
2017-12-07 09:14:42 -0500

    Add UI and functionality to login page

--- angular/src/app/login/login.component.ts
+++ angular/src/app/login/login.component.ts
@@ -1,9 +1,53 @@
 import { Component } from '@angular/core';
+import {
+  FormsModule,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
   title = 'login';
+
+  constructor(private http: Http) { }
+
+  username = new FormControl(
+    '',
+    [
+      Validators.required,
+      Validators.minLength(6),
+      Validators.maxLength(20),
+    ]
+   );
+
+   password = new FormControl(
+    '',
+    [
+      Validators.required,
+      Validators.minLength(10),
+      Validators.maxLength(50),
+    ]
+  );
+
+  submit() {
+    this.http
+      .post('http://35.227.48.112/api/login',
+      {
+        username: this.username.value,
+        password: btoa(this.password.value),
+      })
+      .subscribe(data => {
+        console.log(data);
+      });
+    console.log();
+    return;
+  }
 }
