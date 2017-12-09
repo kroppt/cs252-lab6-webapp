8cf7a98cf366a138a2d637f5df241d81fb87b83b
Tyler D Kropp <kroppt@purdue.edu>
2017-12-09 00:03:00 -0500

    Fix Access-Control-Allow-Origin client errors

--- http/http.go
+++ http/http.go
@@ -3,12 +3,14 @@ package http
 import (
 	"log"
 	"net/http"
+
+	"github.com/rs/cors"
 )
 
 // StartServer will start the server using the given port string.
 // Example: http.StartServer(":8080")
 func StartServer(p string) {
-	r := newRouter()
+	r := cors.Default().Handler(newRouter())
 	checkError(http.ListenAndServe(p, r))
 }
 
